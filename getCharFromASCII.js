bcdf1ff48466b58ff43c1193ae5f28f7e8dc5c00
mverzi <maggie.verzi@gmail.com>
2022-12-01 18:43:31 -0500

    First commit

---
+++ getCharFromASCII.js
@@ -0,0 +1,8 @@
+/**
+ * This function returns the ASCII character associated with the input integer
+ * @param {integer} c 
+ * @returns ASCII character
+ */
+function getChar(c){
+    return String.fromCharCode(c)
+  }
